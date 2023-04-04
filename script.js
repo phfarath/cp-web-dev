a262fad81869bfa846a43ab6c42bd5f1b77d3281
phfarath <pedropontesfarath@gmail.com>
2023-04-04 11:50:16 -0300

    cp-web-dev

---
+++ script.js
@@ -0,0 +1,8 @@
+let seunome = prompt("Digite seu nome: ")
+let suaidade = prompt("Digite sua idade: ")
+let curso = prompt("Digite seu curso atual: ")
+let ano = prompt("Digite o ano atual: ")
+document.write("<br>O seu nome é: ", seunome)
+document.write("<br>A sua idade é: ", suaidade)
+document.write("<br>O seu curso é: ", curso)
+document.write("<br>O ano atual é: ", ano)
